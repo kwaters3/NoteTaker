c78cfadb3525eae31ef65c3a77cc80b504b604e3
kwaters3 <knickler3@gmail.com>
2023-09-24 17:09:59 -0400

    updated server.js to include get api/notes, also updated index html page to direct to notes html once button is clicked - successfully linked localhost:3000 when npm start is invoked

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-const express = require("express")
+// Importing the package
+const express = require("express");
+const fs = require("fs");
 
+// Variables 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 
+app.get('/api/notes', (req, res) => {
+  const note = {
+    name: "note taker one o one",
+    description: "text will go here"
+  }
+  res.json(note)
+})
 
 
 
 
 
-
-app.use(express.static('./public'))
-
+app.use(express.static('public'))
 
 
 
-app.listen(PORT,()=> {
+
+app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`)
 })

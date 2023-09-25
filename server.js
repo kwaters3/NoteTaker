dd2b724737457a5a52e083712a18cb0c31b22a37
kwaters3 <knickler3@gmail.com>
2023-09-25 19:14:14 -0400

    updated server.js to route - get indvidual id by inputing info in the body or retrieving preset notes.

--- server.js
+++ server.js
@@ -27,6 +27,36 @@ app.get('/api/notes', (req, res) => {
   return res.json(notesData) 
 });
 
+//Gets/ **Reads** by id: Title  
+app.get('/api/notes/:title', (req, res) => { 
+  console.log(`${req.method} request received to get note!`);
+  let response;
+  const filterResult = notesData.filter(x => x.title === req.params.title)
+
+// Route from Body input ...still TESTING/DEbugging - res.json note response works in postman 
+  if (req.body && req.body.title) {
+    response = {
+      status: 'success',
+      data: req.body,
+    };
+    res.json(`Note Title: '${response.data.title}', is available`);
+    
+// Route with input from consumer ****THIS gets/reads THE TITLE using PARAMS Title id from DB.JSon file - throws 404 error code if the title name is incorrect when tested on Postman
+  } else if (filterResult.length === 0 ? res.statusCode = 404 : res.statusCode = 200) {
+    res.json(filterResult)
+
+  } else {
+    res.json('Request body must at least contain a title and text');
+  }
+});
+
+
+// Get index.html ** NEED TO DEBUG TO HTML FILE
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+
 // Post/ **CREATE** Note with title and text input through Postman - This allows the user to input data into the body of POSTman
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
@@ -43,19 +73,6 @@ app.post('/api/notes', (req, res) => {
     res.json('Request body must at least contain a title and text');
   }
 })
-
-// Route with input from consumer ****THIS gets/reads THE TITLE using PARAMS Title id from DB.JSon file - throws 404 error code if the title name is incorrect when tested on Postman
-app.get('/api/notes/:title', (req, res) => {
-  const filterResult = notesData.filter(x => x.title === req.params.title)
-  filterResult.length === 0 ? res.statusCode = 404 : res.statusCode = 200
-  res.json(filterResult)
-});
-
-
-// Get index.html ** NEED TO DEBUG TO HTML FILE
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
 
 
 // Listen() method used to listen for incoming connections on the specified port

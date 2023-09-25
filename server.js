84f332d7ee78c1167908b9bf01429cd001aec315
kwaters3 <knickler3@gmail.com>
2023-09-24 21:38:15 -0400

    successfully connected api/notes to db.json file testing through postman

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Importing the packages
 const express = require("express");
 const fs = require("fs");
+const notesData = require('./db/db.json');
 
 
 // Variables 
@@ -12,27 +13,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 
-// Get notes.html
-app.get('/notes', (req, res) => 
+// Get notes.html - NEED TO DEBUG TO HTML FILE
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-// Get index.html
-app.get('*', (req, res) => 
+
+// Get notes through db.json *****THIS WORKS ON POSTMAN
+app.get('/api/notes', (req, res) => { res.json(notesData) });
+
+
+// Get index.html ** NEED TO DEBUG TO HTML FILE
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
-
-
-// app.get('/api/notes', (req, res) => {
-//   const note = {
-//     noteTitle: "note taker one o one",
-//     noteText: "text will go here"
-//   }
-//   res.json(note)
-// });
 
 
 // Listen() method used to listen for incoming connections on the specified port
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`)
-})
+});

c264250d77216c0fa45c5362b691ebfad258d8b7
kwaters3 <knickler3@gmail.com>
2023-09-28 16:27:47 -0400

    add post and delete - deugged index.js file to function properly - app is functioning properly with API, now to deploy to Heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const generateUniqueId = require('generate-unique-id');
 const notesData = require('./db/db.json');
 
+
 // Express Data Parsing - Initialize app Variables
 const app = express();
 
@@ -24,31 +25,98 @@ app.get('/notes', (req, res) => {
 });
 
 
+
 // Gets/**READS** ALL notes from db.json *****THIS WORKS ON POSTMAN and can be viewed in console 
 app.get('/api/notes', (req, res) => {
-  console.log(`${req.method} request received to view all notes via RESTful API `);
   res.json(notesData)
 });
 
 
+
 // EXTRA 
-//Gets/ **Reads** by id: Title  
+//Gets/ **Reads** by id: Title using API 
 app.get('/api/notes/:title', (req, res) => {
   console.log(`${req.method} request received to view note titled: ${req.params.title}`);
   let result;
   result = notesData.find(x => x.title.toLowerCase() === req.params.title.toLowerCase())
-  result ? res.json(result) : res.status(404).json({ message: `Note titled, ${req.params.title}, is not available` })
+  result
+    ? res.json(result)
+    : res.status(404).json({ message: `Note titled, ${req.params.title}, is not available` })
 });
 
 
 
+// Post/ **CREATE** Note 
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+
+  if (title && text) {
+
+    const newNote = {
+      title,
+      text,
+      id: generateUniqueId(),
+    };
+    notesData.push(newNote);
+    fs.writeFile('db/db.json', JSON.stringify(notesData, null, 4), (err) =>
+      err
+        ? console.error(err)
+        : console.log(`Your Note: "${newNote.title}" was added to the list!`));
+
+    response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(200).json(response);
+  } else {
+    res.status(404).json('Error!')
+  }
+});
+
+
+
+
+// BONUS
+// Delete - **DELETE** 
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const delNote = notesData.findIndex(note => note.id == id);
+
+  notesData.splice(delNote, 1);
+  fs.writeFile('db/db.json', JSON.stringify(notesData, null, 4), (err) =>
+    err
+      ? console.error(err)
+      : console.log(`Your note was deleted from the list!`));
+  return res.send();
+});
+
+
+
+// Get index.html - Wildcard - Catch All
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+})
+
+
+// Listen() method used to listen for incoming connections on the specified port
+app.listen(PORT, () => {
+  console.log(`Server started listening on http://localhost:${PORT}`)
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 // DEBUGGING FOR LATER USE:
 // app.get('/api/notes/:title', (req, res) => {
 //   console.log(`${req.method} request received to get note!`);
 //   let response;
 //   const filterResult = notesData.filter(x => x.title === req.params.title)
 
-// // Route from Body input ...still TESTING/DEbugging - res.json note response works in postman 
+// // Route from Body input ...still TESTING/DEbugging - res.json note response works in postman
 //   if (req.body && req.body.title) {
 //     response = {
 //       status: 'success',
@@ -64,53 +132,3 @@ app.get('/api/notes/:title', (req, res) => {
 //     res.json('Request body must at least contain a title and text');
 //   }
 // });
-
-
-
-// Post/ **CREATE** Note 
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-
-  const { title, text } = req.body;
-
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-
-    }
-  }
-
-
-
-  // Bonus feature to use via API body
-  let response;
-
-  if (req.body && req.body.title) {
-    response = {
-      status: 'success',
-      data: req.body,
-    };
-
-    notesData.push(req.body)
-    res.json(`Note Title: '${response.data.title}', is added to the list!`);
-  } else {
-    res.json('Request body must contain a title and text');
-  }
-})
-
-
-// BONUS
-// Delete - **DELETE** 
-
-
-// Get index.html - Wildcard - Catch All
-app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
-})
-
-
-// Listen() method used to listen for incoming connections on the specified port
-app.listen(PORT, () => {
-  console.log(`Server started listening on http://localhost:${PORT}`)
-});
